ea132b2d449c6bfe5fbbb3f8c7e976aa8b019522
Anastasiia0905 <internic@MacBook.local>
2019-03-30 19:55:26 +0200

    M

---
+++ app/script.js
@@ -0,0 +1,6 @@
+/*window.onscroll = function(){
+    const title = document.querySelector('.header__h');
+    const scroll = pageYOffset;
+   title.style.transform = "translate(0px, " + scroll/2 +"%)";
+    
+}*/
